c329ca9650af5d14ae486eaad65317b9d29332be
Pawel Biel <pawel.biel@inbox.com>
2014-04-09 07:12:58 +0200

    100% rspec tests for slides_controller

--- spec/controllers/slides_controller_spec.rb
+++ spec/controllers/slides_controller_spec.rb
@@ -5,6 +5,7 @@ describe SlidesController do
 
   let(:album) { create(:album) }
   let(:slide) { build(:slide) }
+  let(:valid_attributes) { attributes_for(:slide) }
 
   describe "GET #index" do
     before do
@@ -29,19 +30,75 @@ describe SlidesController do
   end
 
   describe "GET #show" do
-    pending
+    before do
+      slide.save
+      get :show, id: slide
+    end
+    it { expect(assigns(:slide)).to eq Slide.last }
+    it { expect(response).to render_template :show }
   end
 
   describe "POST #create" do
-    before { post :create, album_id: album.id, slide: attributes_for(:slide_array) }
-    it { expect(response).to redirect_to(assigns[:album_slides]) }
+    let(:send_post) { post :create, album_id: album.id, slide: attributes_for(:slide_array) }
+    
+    context "success" do
+      it { expect { send_post }.to change(Slide, :count).by(1) } 
+      context do
+        before { send_post }
+        it { expect(response).to redirect_to album_slides_url }
+        it { expect(flash[:notice]).to_not be_nil }
+      end
+    end
+
+    context "failure" do
+      before do
+        @album = album
+        Slide.any_instance.stub(:save).and_return(false)
+      end
+      
+      it { expect{ send_post }.to_not change(Slide, :count) }
+      context do
+        before { send_post }
+        it { expect(response).to redirect_to album_slides_url }
+        it { expect(flash[:error]).to_not be_nil }
+      end
+    end    
   end
 
   describe "PATCH #update" do
-    pending
+    before { @slide = create(:slide, description: 'foo') }
+
+    let(:send_patch) do
+      patch :update, id: @slide, slide: { description: 'bar' }
+      @slide.reload
+    end
+
+    context "success" do
+      it { expect{ send_patch}.to change(@slide, :description).from('foo').to('bar') }
+
+      context do
+        before { send_patch }
+        it { expect(response).to redirect_to album_slides_url(@slide.album_id) }
+        it { expect(flash[:notice]).to_not be_nil }
+      end
+    end
+
+    context "failure" do
+      before do
+        Slide.any_instance.stub(:update).and_return(false)
+      end
+
+      it { expect{ send_patch }.to_not change(@slide, :description) }
+      context do
+        before { send_patch }
+        it { expect(response).to render_template :edit }
+        it { expect(flash[:error]).to_not be_nil }
+      end
+    end
+
   end
 
-  describe "DELETE #destroy" do
+  describe "old DELETE #destroy" do
     before do
       @slide = create(:slide)
       expect(Slide.exists?(@slide)).to be_true
@@ -51,5 +108,34 @@ describe SlidesController do
       expect(Slide.exists?(@slide)).to be_false
     end
   end
-end
 
+  describe "DELETE #destroy" do
+    before do
+      @slide = create(:slide)
+    end
+
+    let(:send_delete) { delete :destroy, id: @slide }
+
+    context "success" do
+      it { expect{ send_delete }.to change(Slide, :count).by(-1) }
+
+      context do
+        before { send_delete }
+        it { expect(response).to redirect_to album_slides_url(@slide.album_id) }
+        it { expect(flash[:notice]).to_not be_nil }
+      end
+    end
+
+    context "failure" do
+      before do
+        Slide.any_instance.stub(:destroy).and_return(false)
+      end
+      it { expect{ send_delete }.to_not change(Slide, :count) }
+
+      context do
+        before { send_delete }
+        it { expect(flash[:error]).to_not be_nil }
+      end
+    end
+  end
+end
