1dd8940fc38712840f3f86c4781ed8b6ea8af4a1
Pawel Biel <pawel.biel@inbox.com>
2014-05-12 20:22:35 +0200

    Devise: users can log in with username rather than with an email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
-	include ApplicationHelper
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  include ApplicationHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :email
+  end
 end
