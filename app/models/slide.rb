392f782804466928176998c576778455f56c2474
Pawel Biel <pawel.biel@inbox.com>
2014-12-22 01:25:14 +0100

    paperclip: turn off url timestamp suffix

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -6,12 +6,13 @@
     :preserve_files => true,
     :styles => { :medium => "800x800>", :small => "300x300>", :thumb => "150x150>" },
     :convert_options => { :medium => "-quality 70 -interlace Plane -strip", :small => "-quality 70 -interlace Plane -strip", :thumb => "-quality 70 -interlace Plane -strip" },
-    :default_url => "/images/coming_soon.png"
+    :default_url => "/images/coming_soon.png",
+    :use_timestamp => false
     validates_attachment_content_type :photo, :content_type => %w(image/jpeg image/jpg)
     validates :photo, presence: true
     validates :photo_fingerprint, uniqueness: true
 
-    process_in_background :photo, queue: 'paperclip_processing'
+    # process_in_background :photo, queue: 'paperclip_processing'
 
   	# validates :album_id, presence: true
   end
