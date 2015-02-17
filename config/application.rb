a316d9ddbdc8c54641b55bda05c3f16842c230b1
Pawel Biel <pawel.biel@inbox.com>
2015-02-17 23:43:42 +0100

    add slides_index.js to the assets

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ Bundler.require(:default, Rails.env)
 
 module Footoo
   class Application < Rails::Application
+    config.assets.precompile += ['slides_index.js']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
